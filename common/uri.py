735eccea0f8d977352048571b2479b3f16b8dd18
Michael <mrl@nottres.com>
2016-11-19 22:39:58 +0800

    Fix dir index

--- common/uri.py
+++ common/uri.py
@@ -12,33 +12,37 @@ class uri:
             type = 'dir'
         else:
             type = 'document'
+
         if scratch:
             if scratch == 'index':
                 if config.uri_extname:
-                    request_uri = request_uri + 'index.md'
+                    request_uri += 'index.md'
                 else:
-                    request_uri = request_uri + 'index'
+                    request_uri += 'index'
                 type = 'document'
             elif scratch == 'static':
-                self.path = config.static_folder + '/' + request_uri
-                self.exists = isfile(self.path)
+                path = config.static_folder + '/' + request_uri
+                exists = isfile(path)
                 type = 'static'
                 request_uri = 'static' + request_uri
-        self.request_uri = request_uri
+
         if type == 'document':
             if config.uri_extname:
-                self.path = config.doc_root + '/' + request_uri
+                path = config.doc_root + '/' + request_uri
             else:
-                self.path = config.doc_root + '/' + request_uri + '.md'
-            self.type = 'document'
-            self.exists = isfile(self.path)
-            if not self.exists:
+                path = config.doc_root + '/' + request_uri + '.md'
+            exists = isfile(path)
+            if not exists:
                 if isdir(config.doc_root + '/' + request_uri):
                     type = 'dir'
+                    if not request_uri.endswith('/'): request_uri += '/'
         if type == 'dir':
-            self.path = config.doc_root + '/' + request_uri
-            self.type = 'dir'
-            self.exists = isdir(self.path)
+            path = config.doc_root + '/' + request_uri
+            exists = isdir(path)
+        self.request_uri = request_uri
+        self.type = type
+        self.path = path
+        self.exists = exists
 
 import sys
 sys.modules[__name__] = uri
